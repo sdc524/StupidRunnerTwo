f0d9ed2ddc95289a73423fa8f26a74faf4f47e57
Stephen Cassat <Stephencassat@yahoo.com>
2018-09-10 09:57:36 -0600

    Added boolean code

---
+++ src/controller/SecondStupidRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class SecondStupidRunner 
+{
+	public static void main (String [] args)
+	{
+	StupidController app = new StupidController();
+	app.start();
+	}
+	
+}
---
+++ src/controller/StupidController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class StupidController 
+{
+
+	public void start()
+	{
+		System.out.println("My program can speak to me!");
+	}
+}
+	
